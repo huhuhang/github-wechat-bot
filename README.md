3de884719645e645501723b039e51f2113353a87
huhuhang <huhuhang@gmail.com>
2021-12-29 10:34:15 +0800

    remove commits

---
+++ README.md
@@ -0,0 +1,43 @@
+# GitHub WeChat Bot
+
+本项目提供了基于 Cloudflare Workers 部署 API，支持基于 GitHub Webhook 将操作消息推送给企业微信机器人。
+
+目前支持：
+
+- Issues 相关操作提醒。
+- Pull Requests 相关操作提醒。
+- 欢迎贡献支持更多操作提醒。
+
+## 如何部署
+
+你可以直接在 Cloudflare Workers 网页版中新建项目，并复制本仓库中的 `index.js` 到在线编辑器中部署。
+
+或者使用官方提供的 wrangler cli 工具：
+
+1. 了解 Cloudflare Workers 命令行工具 [wrangler](https://github.com/cloudflare/wrangler) 的使用方法。
+2. 基于本项目初始化一个新的项目。
+
+    ```bash
+    wrangler generate github-wechat-bot https://github.com/huhuhang/github-wechat-bot
+    ```
+
+3. 请修改 `wrangler.toml` 中预留的 `account_id`，替换为你的账户信息。
+4. 将 `github-wechat-bot` 项目添加到 Cloudflare Workers 部署。
+
+## 如何使用
+
+1. 基于上述步骤在 Cloudflare Workers 中部署完成后，你会得到类似 `https://github-wechat-bot.你的自定义域名.workers.dev` 的路由地址。
+
+    ![1640744784436](https://cdn.jsdelivr.net/gh/huhuhang/cdn@master/images/2021/12/1640744784436.png)
+
+2. 创建企业微信机器人，并获得 Webhook Key。
+
+    ![1640744769818](https://cdn.jsdelivr.net/gh/huhuhang/cdn@master/images/2021/12/1640744769818.png)
+
+3. 此时 Webhook 推送地址为：`https://github-wechat-bot.你的自定义域名.workers.dev/?key=你的企业微信机器人KEY`
+
+4. 在 GitHub 上配置 Webhook，将 Webhook 的 URL 替换为上述路由地址。注意选择 `application/json` 类型，下方推送消息类别可以包含目前支持的 Issues，Pull requests。
+
+    ![1640744749518](https://cdn.jsdelivr.net/gh/huhuhang/cdn@master/images/2021/12/1640744749518.png)
+
+5. 如果一切顺利，你会在企业微信群内收到 PING 请求。
---
+++ index.js
@@ -0,0 +1,158 @@
+/**
+ * 
+ * @param {JSON} response 处理JSON格式的响应
+ * @returns 
+ */
+async function gatherResponse(response) {
+  const { headers } = response
+  const contentType = headers.get("content-type") || ""
+  if (contentType.includes("application/json")) {
+    return JSON.stringify(await response.json())
+  }
+}
+
+/**
+ * 
+ * @param {String} botKey 企业微信机器人密钥
+ * @param {String} content 需要发送的内容，支持 Markdown 格式
+ * @returns 
+ */
+async function sendMdMsg(botKey, content) {
+  const baseUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/"
+  const url = `${baseUrl}send?key=${botKey}`
+  const init = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "msgtype": "markdown",
+      "markdown": {
+        "content": content
+      }
+    })
+  }
+  const response = await fetch(url, init)
+  return await gatherResponse(response)
+}
+
+// 标记事件动作
+const actionWords = {
+  "opened": "创建",
+  "closed": "关闭",
+  "reopened": "重新发起",
+  "edited": "更新",
+  "merge": "合并",
+  "created": "创建",
+  "requested": "请求",
+  "completed": "完成",
+  "synchronize": "同步更新"
+};
+
+/**
+ * 处理 Ping 请求
+ * @param {String} botKey 企业微信机器人密钥
+ * @param {JSON} reqBody GitHub 传递的请求体
+ * @returns 
+ */
+async function handlePing(botKey, reqBody) {
+  const { repository } = reqBody;
+  const mdMsg = "成功收到了来自 Github 的 Ping 请求，仓库地址: " + repository.html_url;
+  return await sendMdMsg(botKey, mdMsg);
+}
+
+/**
+ * 处理 PR 请求
+ * @param {String} botKey 企业微信机器人密钥
+ * @param {JSON} reqBody GitHub 传递的请求体
+ * @returns 
+ */
+async function handlePR(botKey, reqBody) {
+  const { action, sender, pull_request, repository } = reqBody;
+  if (sender.type !== "Bot") {
+    if (action == "opened" || action == "reopened") {
+      const mdMsg = `${sender.login} 在 [${repository.full_name}](${repository.html_url}) <font color="info">${actionWords[action]}</font>了一个 PR:
+      > 分支: ${pull_request.head.ref} → ${pull_request.base.ref}
+      > 名称: [${pull_request.title}](${pull_request.html_url})
+      > 修改: ${pull_request.changed_files} 个文件 (<font color="info">+ ${pull_request.additions}</font> <font color="warning">- ${pull_request.deletions}</font> 行修改)`;
+      return await sendMdMsg(botKey, mdMsg);
+    }
+    else if (action == "closed" && pull_request.merged) {
+      const mdMsg = `${sender.login} 在 [${repository.full_name}](${repository.html_url}) <font color="warning">合并</font>了一个 PR:
+      > 分支: ${pull_request.head.ref} → ${pull_request.base.ref}
+      > 名称: [${pull_request.title}](${pull_request.html_url}) (${pull_request.user.login} 发起)
+      > 修改: ${pull_request.changed_files} 个文件 (<font color="info">+ ${pull_request.additions}</font> <font color="warning">- ${pull_request.deletions}</font> 行修改)
+      > 审核: ${pull_request.merged_by.login} (${pull_request.review_comments} 条意见)`;
+      return await sendMdMsg(botKey, mdMsg);
+    }
+    else {
+      return `发起、关闭、重新发起的事件才会被处理`;
+    }
+  } else {
+    return `机器人操作不会被处理`;
+  }
+}
+
+/**
+ * 处理 Issues 请求
+ * @param {String} botKey 企业微信机器人密钥
+ * @param {JSON} reqBody GitHub 传递的请求体
+ * @returns 
+ */
+async function handleIssue(botKey, reqBody) {
+  const { action, issue, repository } = reqBody;
+  if (action == "opened" || action == "closed" || action == "reopened") {
+    const mdMsg = `${sender.login}  在 [${repository.full_name}](${repository.html_url}) <font color="info">${actionWords[action]}了一个 Issues</font>:
+    > 名称: [${issue.title}](${issue.html_url})`;
+    return await sendMdMsg(botKey, mdMsg);
+  }
+  else {
+    return `发起、关闭、重新发起的事件才会被处理`;
+  }
+}
+
+/**
+ * 
+ * @param {JSON} request GitHub 传递的请求
+ * @returns 
+ */
+async function handleRequest(request) {
+  const { searchParams } = new URL(request.url)
+  // 从 URL 获取传入的机器人密钥
+  let botKey = searchParams.get('key')
+  // 从请求中获取消息内容
+  var reqBody = await gatherResponse(request)
+  // 解析 GitHub 传递的消息类型
+  const gitEvent = request.headers.get("X-GitHub-Event")
+  console.log(`收到了一个 ${gitEvent} 事件`)
+  switch (gitEvent) {
+    // 如果是 Ping 事件
+    case "ping":
+      var results = await handlePing(botKey, JSON.parse(reqBody))
+      break;
+    // 如果是 PR 事件
+    case "pull_request":
+      var results = await handlePR(botKey, JSON.parse(reqBody))
+      break;
+    // 如果是 Issues 事件
+    case "issues":
+      var results = await handleIssue(botKey, JSON.parse(reqBody))
+      break;
+    // 其他事件暂不支持
+    default:
+      var results = `暂不支持处理 ${gitEvent} 事件`
+      break;
+  }
+  return new Response(results)
+}
+
+addEventListener("fetch", event => {
+  const { request } = event
+  // 仅处理 POST 请求
+  if (request.method === "POST") {
+    return event.respondWith(handleRequest(request))
+  }
+  else {
+    return event.respondWith(new Response(`Please use the POST method.`))
+  }
+})
